ccf40cc012f659bbd03d904e81de60aad29350bd
NikoGkot <nikogkot@gmail.com>
2024-05-26 14:22:27 +0200

    Configuration

--- src/main/kotlin/com/kotlinspring/config/Configuration.kt
+++ src/main/kotlin/com/kotlinspring/config/Configuration.kt
@@ -1,9 +1,43 @@
 package com.kotlinspring.config
 
+import com.kotlinspring.repository.UserRepository
+import com.kotlinspring.service.CustomUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
 class Configuration {
+
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService {
+        return CustomUserDetailsService(userRepository)
+    }
+
+    @Bean
+    fun encoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider {
+        return DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepository))
+                it.setPasswordEncoder(encoder())
+            }
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
+    }
 }
