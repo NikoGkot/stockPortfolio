e006f49bf210cc5bd625540258209f115d5908cd
NikoGkot <nikogkot@gmail.com>
2024-05-26 14:07:49 +0200

    Add JWT properties and Token service

---
+++ src/main/kotlin/com/kotlinspring/config/JwtProperties.kt
@@ -0,0 +1,11 @@
+package com.kotlinspring.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("jwt")
+class JwtProperties (
+    val key: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long
+){
+}
